70ef118e9c78627b9ea968f6af3a1ee2e85cc0b2
fechan <fred@fredchan.org>
2024-12-22 23:49:22 -0800

    add editing for subtriangles

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -38,12 +38,24 @@ function updateEditor(code) {
     updateEditorField(sentence, triangleElem, triangleNbr, 'word');
     updateEditorField(sentence, triangleElem, triangleNbr, 'color');
     updateEditorField(sentence, triangleElem, triangleNbr, 'level');
+    updateEditorField(sentence, triangleElem, triangleNbr, 'dot');
 
-    if (triangle.color === 'blue') {
-      updateEditorField(sentence, triangleElem, triangleNbr, 'dot');
-    } else {
+    if (triangle.color !== 'blue') {
       const dot = triangleElem.querySelector('.editor-dot');
       dot.style.display = 'none';
+    }
+
+    const [subtriangleLabel, subtriangleList] = triangleElem.querySelector(`.editor-subtriangle-list`).children;
+    subtriangleLabel.setAttribute('for', `subtriangles-${triangleNbr}`);
+    subtriangleList.setAttribute('name', `subtriangles-${triangleNbr}`);
+    for (let [subtriangleNbr, subtriangle] of triangle.triangles.entries()) {
+      const subtriangleElem = editorSubtriangle.content.cloneNode(true);
+
+      updateSubtriangleField(sentence, subtriangleElem, triangleNbr, subtriangleNbr, 0);
+      updateSubtriangleField(sentence, subtriangleElem, triangleNbr, subtriangleNbr, 1);
+      updateSubtriangleField(sentence, subtriangleElem, triangleNbr, subtriangleNbr, 2);
+
+      subtriangleList.appendChild(subtriangleElem);
     }
 
     editor.appendChild(triangleElem);
@@ -58,6 +70,19 @@ function updateEditorField(sentence, triangleElem, triangleNbr, fieldName) {
 
   input.addEventListener('change', (e) => {
     sentence[triangleNbr][fieldName] = (input.type === 'number') ? e.target.valueAsNumber : e.target.value;
+    trilangleCode = JSON.stringify(sentence);
+    updateEverything();
+  });
+}
+
+function updateSubtriangleField(sentence, subtriangleElem, triangleNbr, subtriangleNbr, fieldName) {
+  const [label, input] = subtriangleElem.querySelector(`.editor-${fieldName}`).children;
+  label.setAttribute('for', `${fieldName}-${triangleNbr}-${subtriangleNbr}`);
+  input.setAttribute('name', `${fieldName}-${triangleNbr}-${subtriangleNbr}`);
+  input.value = sentence[triangleNbr].triangles[subtriangleNbr][fieldName];
+
+  input.addEventListener('change', (e) => {
+    sentence[triangleNbr].triangles[subtriangleNbr][fieldName] = e.target.valueAsNumber;
     trilangleCode = JSON.stringify(sentence);
     updateEverything();
   });
