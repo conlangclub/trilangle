e51f89dd7b2cd809a26a3750985b2686f402d47b
fechan <fred@fredchan.org>
2024-12-22 23:26:48 -0800

    populate fancy editor static fields

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,11 +1,20 @@
 const input = document.getElementById("input");
 const preview = document.getElementById("preview");
 
-input.addEventListener("input", () => render(input.value));
-render(input.value);
+const editor = document.getElementById("fancy-editor");
+const editorTriangle = document.getElementById("editor-triangle");
+const editorSubtriangle = document.getElementById("editor-subtriangle");
+
+let trilangleCode = input.value;
+
+input.addEventListener("input", () => {
+  render(trilangleCode);
+  updateEditor(trilangleCode);
+});
+render(trilangleCode);
+updateEditor(trilangleCode);
 
 function render(code) {
-  console.log("Asdf")
   let template = document.createElement("template");
   template.innerHTML = code;
   
@@ -14,6 +23,35 @@ function render(code) {
   
   preview.innerHTML = "";
   preview.append(sentence);
+}
+
+function updateEditor(code) {
+  const sentence = JSON.parse(code);
+  editor.innerHTML = '';
+
+  for (let [triangleNbr, triangle] of sentence.entries()) {
+    const triangleElem = editorTriangle.content.cloneNode(true);
+
+    updateEditorField(triangleElem, triangleNbr, 'word', triangle.word);
+    updateEditorField(triangleElem, triangleNbr, 'color', triangle.color);
+    updateEditorField(triangleElem, triangleNbr, 'level', triangle.level);
+
+    if (triangle.color === 'blue') {
+      updateEditorField(triangleElem, triangleNbr, 'dot', triangle.dot);
+    } else {
+      const dot = triangleElem.querySelector('.editor-dot');
+      dot.style.display = 'none';
+    }
+
+    editor.appendChild(triangleElem);
+  }
+}
+
+function updateEditorField(triangleElem, triangleNbr, fieldName, value) {
+  const [label, input] = triangleElem.querySelector(`.editor-${fieldName}`).children;
+  label.setAttribute('for', `${fieldName}-${triangleNbr}`);
+  input.setAttribute('name', `${fieldName}-${triangleNbr}`);
+  input.value = value;
 }
 
 input.addEventListener('keydown', function(e) {
